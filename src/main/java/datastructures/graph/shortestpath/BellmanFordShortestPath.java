00478e3eb3f119d7665e197c74cfd14fd563adfe
r0m09yr <r0m09yr@homeoffice.wal-mart.com>
2022-08-26 22:02:21 +0530

    Refactoring

--- src/main/java/datastructures/graph/shortestpath/BellmanFordShortestPath.java
+++ src/main/java/datastructures/graph/shortestpath/BellmanFordShortestPath.java
@@ -19,12 +19,12 @@ public class BellmanFordShortestPath {
             return null;
 
         Map<List<Vertex<T>>, Integer> result = new HashMap<>();
-        Map<Vertex<T>, Integer> shortestWeightForVertex = new HashMap<>();
+        Map<Vertex<T>, Integer> minWeightToReachVertex = new HashMap<>();
         Map<Vertex<T>, Vertex<T>> parents = new HashMap<>();
 
         for (Vertex<T> v : graph.getAllVertexes())
-            shortestWeightForVertex.put(v, INF);
-        shortestWeightForVertex.put(start, 0);
+            minWeightToReachVertex.put(v, INF);
+        minWeightToReachVertex.put(start, 0);
         parents.put(start, null);
 
         int totalVertexes = graph.getAllVertexes().size();
@@ -34,12 +34,12 @@ public class BellmanFordShortestPath {
                 Vertex<T> u = edge.getVertex1();
                 Vertex<T> v = edge.getVertex2();
 
-                int U = shortestWeightForVertex.get(u);
-                int V = shortestWeightForVertex.get(v);
+                int U = minWeightToReachVertex.get(u);
+                int V = minWeightToReachVertex.get(v);
                 int UV = edge.getWeight();
 
                 if (U + UV < V) {
-                    shortestWeightForVertex.put(v, U + UV);
+                    minWeightToReachVertex.put(v, U + UV);
                     parents.put(v, u);
                 }
             }
@@ -48,12 +48,12 @@ public class BellmanFordShortestPath {
         for (Edge<T> edge : graph.getEdges()) {
             Vertex<T> u = edge.getVertex1();
             Vertex<T> v = edge.getVertex2();
-            if (shortestWeightForVertex.get(u) + edge.getWeight() < shortestWeightForVertex.get(v)) {
+            if (minWeightToReachVertex.get(u) + edge.getWeight() < minWeightToReachVertex.get(v)) {
                 throw new RuntimeException("Graph has a negative weight cycle and hence solution not possible");
             }
         }
 
-        for (Entry<Vertex<T>, Integer> entry : shortestWeightForVertex.entrySet()) {
+        for (Entry<Vertex<T>, Integer> entry : minWeightToReachVertex.entrySet()) {
             List<Vertex<T>> path = new ArrayList<>();
             result.put(path, entry.getValue());
             path.add(entry.getKey());
