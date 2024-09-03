54f4debad674329e2a9a820ddb3c34678d4139c5
rahul.mane08@gmail.com <rahul.mane08@gmail.com>
2024-09-03 14:38:57 +0530

    new code

---
+++ src/main/java/leetcode/trees/medium/TreeIsStrictBT.java
@@ -0,0 +1,19 @@
+package leetcode.trees.medium;
+
+import leetcode.trees.TreeNode;
+
+public class TreeIsStrictBT {
+  public boolean checkIfStrictTree(TreeNode root) {
+    if (root == null) {
+      return true;
+    }
+    int children = 0;
+    if (root.left != null) {
+      ++children;
+    }
+    if (root.right != null) {
+      ++children;
+    }
+    return (children % 2 == 0) && checkIfStrictTree(root.left) && checkIfStrictTree(root.right);
+  }
+}
