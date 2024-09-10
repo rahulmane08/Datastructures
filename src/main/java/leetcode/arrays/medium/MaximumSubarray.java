1d26a3cab14e43a901c64334adc397e15bd77c2e
rahul.mane08@gmail.com <rahul.mane08@gmail.com>
2024-09-10 17:48:15 +0530

    new code

---
+++ src/main/java/leetcode/arrays/medium/MaximumSubarray.java
@@ -0,0 +1,16 @@
+package leetcode.arrays.medium;
+
+public class MaximumSubarray {
+  public int maxSubArray(int[] nums) {
+    if (nums == null || nums.length == 0) {
+      return 0;
+    }
+    int maxSum = nums[0];
+    int currSum = nums[0];
+    for (int i = 1; i < nums.length; i++) {
+      currSum = Math.max(nums[i], currSum + nums[i]);
+      maxSum = Math.max(maxSum, currSum);
+    }
+    return maxSum;
+  }
+}
