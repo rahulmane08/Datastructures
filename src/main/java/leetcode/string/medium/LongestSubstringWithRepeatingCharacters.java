fdf6795eccd205fbf1cade1042a1c2033738ec62
rahul.mane08@gmail.com <rahul.mane08@gmail.com>
2024-09-10 17:44:40 +0530

    new code

---
+++ src/main/java/leetcode/string/medium/LongestSubstringWithRepeatingCharacters.java
@@ -0,0 +1,29 @@
+package leetcode.string.medium;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubstringWithRepeatingCharacters {
+  public int lengthOfLongestSubstring(String s) {
+    if (s == null || s.isEmpty()) {
+      return 0;
+    }
+    int start = 0;
+    int end = 0;
+    int maxLength = 0;
+    Set<Character> set = new HashSet<>();
+    while (end < s.length()) {
+      char right = s.charAt(end);
+      if (!set.contains(right)) {
+        set.add(right);
+        maxLength = Math.max(maxLength, end - start + 1);
+        end++;
+      } else {
+        while (start < end && set.contains(right)) {
+          set.remove(s.charAt(start++));
+        }
+      }
+    }
+    return maxLength;
+  }
+}
