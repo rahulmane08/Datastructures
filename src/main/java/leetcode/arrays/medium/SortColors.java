01ff433c0466bfc809e1da9de5b04eb1d44ad0ff
rahul.mane08@gmail.com <rahul.mane08@gmail.com>
2024-09-10 17:50:05 +0530

    new code

---
+++ src/main/java/leetcode/arrays/medium/SortColors.java
@@ -0,0 +1,25 @@
+package leetcode.arrays.medium;
+
+public class SortColors {
+  public void sortColors(int[] nums) {
+    int p0 = 0;
+    int p2 = nums.length - 1;
+    int curr = 0;
+
+    while (curr <= p2) {
+      if (nums[curr] == 2) {
+        swap(nums, curr, p2--);
+      } else if (nums[curr] == 0) {
+        swap(nums, curr++, p0++);
+      } else {
+        curr++;
+      }
+    }
+  }
+
+  public static void swap(int[] arr, int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+}
