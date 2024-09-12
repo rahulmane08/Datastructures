758bfd0a3bfb5959158e65a20297046a7cfb1db5
rahul.mane08@gmail.com <rahul.mane08@gmail.com>
2024-09-12 11:48:58 +0530

    new code

---
+++ src/main/java/leetcode/backtracking/medium/PermutationInString.java
@@ -0,0 +1,39 @@
+package leetcode.backtracking.medium;
+
+/**
+ * https://leetcode.com/problems/permutation-in-string/description/
+ */
+public class PermutationInString {
+  public boolean checkInclusion(String s1, String s2) {
+    return check(s2, s1.toCharArray(), 0, s1.length() - 1);
+  }
+
+  boolean check(String target, char[] chars, int start, int end) {
+    if (start == end && target.contains(new String(chars))) {
+      return true;
+    }
+    for (int i = start; i <= end; i++) {
+      swap(chars, i, start);
+      boolean check = check(target, chars, start + 1, end);
+      swap(chars, i, start);
+      if (check) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  void swap(char[] chars, int i, int j) {
+    if (i != j) {
+      char temp = chars[i];
+      chars[i] = chars[j];
+      chars[j] = temp;
+    }
+  }
+
+  public static void main(String[] args) {
+    PermutationInString util = new PermutationInString();
+    System.out.println(util.checkInclusion("ab", "eidbaooo"));
+    System.out.println(util.checkInclusion("ab", "eidboaoo"));
+  }
+}
