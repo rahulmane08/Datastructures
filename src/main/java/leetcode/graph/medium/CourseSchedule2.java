1296b7ce8277c67658117a4ee26403cef4e92f95
rahul.mane08@gmail.com <rahul.mane08@gmail.com>
2024-09-15 16:35:26 +0530

    new code

--- src/main/java/leetcode/graph/medium/CourseSchedule2.java
+++ src/main/java/leetcode/graph/medium/CourseSchedule2.java
@@ -9,29 +9,24 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 
-/**
- * {{1, 0}, {2, 0}, {3, 1}}
- * <p>
- * 0 -> {1, 2}
- * 1 -> {3}
- */
-public class CourseSchedule {
-  public boolean canFinish(int numCourses, int[][] prerequisites) {
+public class CourseSchedule2 {
+  public int[] findOrder(int numCourses, int[][] prerequisites) {
     if (prerequisites == null || prerequisites.length == 0) {
-      return true;
+      return new int[]{0};
     }
+    int[] courseOrder = new int[numCourses];
     Map<Integer, List<Integer>> graph = new HashMap<>();
     Map<Integer, Integer> inDegrees = new HashMap<>();
-    populateGraph(prerequisites, graph, inDegrees);
 
     // bfs queue only for courses with no pre-req, i.e. courses with indegree = 0;
     Queue<Integer> coursesWithNoPrerequisites = new LinkedList<>();
-
     graph.keySet().stream()
         .filter(course -> !inDegrees.containsKey(course)).forEach(coursesWithNoPrerequisites::offer);
+
+    int index = 0;
     while (!coursesWithNoPrerequisites.isEmpty()) {
       Integer currentCourse = coursesWithNoPrerequisites.poll();
-      numCourses--;
+      courseOrder[index++] = currentCourse;
       for (Integer nextCourse : graph.getOrDefault(currentCourse, Collections.emptyList())) {
         Integer inDegree = inDegrees.compute(nextCourse, (c, d) -> d = d - 1);
         if (inDegree == 0) {
@@ -39,25 +34,23 @@ public class CourseSchedule {
         }
       }
     }
-    return numCourses >= 0;
+    return courseOrder;
   }
 
-  Map<Integer, List<Integer>> populateGraph(int[][] prerequisites,
+  int populateGraph(int[][] prerequisites,
                                             final Map<Integer, List<Integer>> graph,
                                             final Map<Integer, Integer> inDegrees) {
-    Arrays.stream(prerequisites)
-        .forEach(a -> {
-          graph.compute(a[1], (pre, courses) -> courses == null ? new ArrayList<>() : courses).add(a[0]);
-          inDegrees.compute(a[0], (course, degree) -> degree == null ? 1 : degree + 1);
-        });
-    return graph;
+    int maxCourses = 0;
+    for (int[] a : prerequisites) {
+      graph.compute(a[1], (pre, courses) -> courses == null ? new ArrayList<>() : courses).add(a[0]);
+      inDegrees.compute(a[0], (course, degree) -> degree == null ? 1 : degree + 1);
+      maxCourses++;
+    }
+    return maxCourses;
   }
 
   public static void main(String[] args) {
-    CourseSchedule util = new CourseSchedule();
-    System.out.println(util.canFinish(2, new int[][] {{0, 1}}));
-    System.out.println(util.canFinish(3, new int[][] {{1, 0}, {2, 1}}));
-    System.out.println(util.canFinish(2, new int[][] {{0, 1}, {1, 0}}));
-    System.out.println(util.canFinish(4, new int[][] {{1, 4}, {2, 4}, {3, 1}, {3, 2}}));
+    CourseSchedule2 util = new CourseSchedule2();
+    System.out.println(Arrays.toString(util.findOrder(4, new int[][] {{1, 0}, {2, 0}, {3, 1}, {3, 2}})));
   }
 }
