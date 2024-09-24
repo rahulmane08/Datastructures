9b40e40f20b4821aeecdb88928acbad52e430a39
rahul.mane08@gmail.com <rahul.mane08@gmail.com>
2024-09-24 20:54:41 +0530

    new code

---
+++ src/main/java/leetcode/arrays/hard/TrappingRainWater.java
@@ -0,0 +1,34 @@
+package leetcode.arrays.hard;
+
+public class TrappingRainWater {
+  public int trap(int[] arr) {
+    if (arr == null) {
+      return 0;
+    }
+
+    int n = arr.length;
+    int[] maxWaterCapacity = new int[n];
+    int max = 0;
+    for (int i = 0; i < n; i++) {
+      max = Math.max(max, arr[i]);
+      maxWaterCapacity[i] = max;
+    }
+
+    max = 0;
+    for (int i = n - 1; i >= 0; i--) {
+      max = Math.max(max, arr[i]);
+      maxWaterCapacity[i] = Math.min(max, maxWaterCapacity[i]);
+    }
+
+    int totalCapacity = 0;
+    for (int i = 0; i < n; i++) {
+      totalCapacity += (maxWaterCapacity[i] - arr[i]);
+    }
+    return totalCapacity;
+  }
+
+  public static void main(String[] args) {
+    TrappingRainWater util = new TrappingRainWater();
+    System.out.println(util.trap(new int[] {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
+  }
+}
