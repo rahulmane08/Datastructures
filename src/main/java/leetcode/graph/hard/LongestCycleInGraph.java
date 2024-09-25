910418dba836c2f8bf8bd430f1227cc599c385a9
rahul.mane08@gmail.com <rahul.mane08@gmail.com>
2024-09-25 23:41:21 +0530

    new code

--- src/main/java/leetcode/graph/hard/LongestCycleInGraph.java
+++ src/main/java/leetcode/graph/hard/LongestCycleInGraph.java
@@ -1,6 +1,7 @@
 package leetcode.graph.hard;
 
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * https://leetcode.com/problems/longest-cycle-in-a-graph/description/?envType=problem-list-v2&envId=topological-sort&difficulty=HARD
@@ -10,36 +11,47 @@ import java.util.HashSet;
  */
 public class LongestCycleInGraph {
   public int longestCycle(int[] edges) {
-    HashSet<Integer> visited = new HashSet<>();
-    int longestCycle = 0;
+    int[] max = new int[] {-1, -1};
+    Map<Integer, Integer> visitCount = new HashMap<>();
     for (int fromVertex = 0; fromVertex < edges.length; fromVertex++) {
-      if (!visited.contains(fromVertex)) {
-        visited.add(fromVertex);
-        longestCycle = Math.max(longestCycle, getCycleLengthUsingDfs(edges, fromVertex, visited));
+      if (!visitCount.containsKey(fromVertex)) {
+        dfs(edges, fromVertex, visitCount, max, 0);
       }
     }
-    return longestCycle;
+    return max[1];
   }
 
 
-  private int getCycleLengthUsingDfs(int[] edges, int vertex,
-                                     HashSet<Integer> visited) {
-    int neighbor = edges[vertex];
-    int length = 0;
-    while (neighbor != -1 && neighbor != vertex && !visited.contains(neighbor)) {
-      length++;
-      neighbor = edges[neighbor];
+  private void dfs(int[] edges,
+                   int vertex,
+                   Map<Integer, Integer> visitCount,
+                   int[] max,
+                   int currentVisit) {
+    if (visitCount.containsKey(vertex)) {
+      int cycleLengthStartingFromVertex = currentVisit - visitCount.get(vertex);
+      if (max[1] < cycleLengthStartingFromVertex) {
+        max[0] = vertex;
+        max[1] = cycleLengthStartingFromVertex;
+      }
+      return;
     }
-    if (neighbor != vertex) {
-      return 0;
+    visitCount.put(vertex, currentVisit);
+    if (edges[vertex] != -1) {
+      dfs(edges, edges[vertex], visitCount, max, currentVisit + 1);
     }
-    return length + 1;
   }
 
   public static void main(String[] args) {
     LongestCycleInGraph util = new LongestCycleInGraph();
     System.out.println(util.longestCycle(new int[] {1, 2, 0}));
     System.out.println(util.longestCycle(new int[] {1, 2, -1}));
-    System.out.println(util.longestCycle(new int[] {3, 3, 4, 2, 3}));
+    System.out.println(util.longestCycle(new int[] {3, 3, 4, 2, 5, 3}));
+    /**
+     *  1
+     *    \
+     *  0 -> 3 - > 2
+     *       |    |
+     *       5  - 4
+     */
   }
 }
