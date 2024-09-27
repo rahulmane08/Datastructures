68f8e30a2d95fcb9e0d99a0f1dced94d25431153
rahul.mane08@gmail.com <rahul.mane08@gmail.com>
2024-09-27 18:06:14 +0530

    new code

--- src/main/java/leetcode/dp/easy/ClimbStairs.java
+++ src/main/java/leetcode/dp/easy/ClimbStairs.java
@@ -2,22 +2,21 @@ package leetcode.dp.easy;
 
 import java.util.Map;
 
-public class Fibonacci {
-  public int fib(int n) {
+public class ClimbStairs {
+  public int climbStairs(int n) {
+    // return topDown(n, new HashMap<>());
     return bottomUp(n);
   }
 
-  /**
-   * T(n) = 2T(n-1) + 1;
-   * O(n) = 2^n
-   *
-   * @param n
-   * @return
-   */
   private int topDown(int n, Map<Integer, Integer> dp) {
-    if (n < 2) {
-      return n;
+    if (n == 0) {
+      return 1; // reached the top.
     }
+
+    if (n < 0) {
+      return 0;
+    }
+
     if (!dp.containsKey(n)) {
       dp.put(n, topDown(n - 1, dp) + topDown(n - 2, dp));
     }
@@ -25,19 +24,21 @@ public class Fibonacci {
   }
 
   private int bottomUp(int n) {
+    if (n == 0) {
+      return 1;
+    }
+
     int[] dp = new int[n + 1];
-    dp[0] = 0;
+    dp[0] = 1;
     dp[1] = 1;
-    for (int i = 2; i < dp.length; i++) {
+    for (int i = 2; i <= n; i++) {
       dp[i] = dp[i - 1] + dp[i - 2];
     }
     return dp[n];
   }
 
   public static void main(String[] args) {
-    Fibonacci util = new Fibonacci();
-    for (int i = 2; i <= 5; i++) {
-      System.out.println(util.fib(i));
-    }
+    ClimbStairs util = new ClimbStairs();
+    System.out.println(util.climbStairs(0));
   }
 }
