8a9fd4fe276ef197651996c5fbedae84776a5282
rahul.mane08@gmail.com <rahul.mane08@gmail.com>
2024-09-27 18:33:30 +0530

    new code

---
+++ src/main/java/leetcode/dp/medium/RobotUniquePaths.java
@@ -0,0 +1,36 @@
+package leetcode.dp.medium;
+
+public class RobotUniquePaths {
+  private int[][] moves = {{0, 1}, {1, 0}}; // R,D
+
+  public int uniquePaths(int m, int n) {
+    Integer[][] dp = new Integer[m][n];
+    dp[m - 1][n - 1] = 1;
+    return topDown(0, 0, m, n, dp);
+  }
+
+  private int topDown(int i, int j, int m, int n, Integer[][] dp) {
+    if (i == m - 1 && j == n - 1) {
+      return 1;
+    }
+    if (dp[i][j] == null) {
+      int totalPaths = 0;
+      for (int[] move : moves) {
+        if (isValid(i + move[0], j + move[1], m, n)) {
+          totalPaths += topDown(i + move[0], j + move[1], m, n, dp);
+        }
+      }
+      dp[i][j] = totalPaths;
+    }
+    return dp[i][j];
+  }
+
+  private boolean isValid(int i, int j, int m, int n) {
+    return 0 <= i && i < m && 0 <= j && j < n;
+  }
+
+  public static void main(String[] args) {
+    RobotUniquePaths util = new RobotUniquePaths();
+    System.out.println(util.uniquePaths(3, 3));
+  }
+}
