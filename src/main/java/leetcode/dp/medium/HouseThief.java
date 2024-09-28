b539c9d4d80fadd995b51e06e0e5cfba66ab6b16
rahul.mane08@gmail.com <rahul.mane08@gmail.com>
2024-09-28 12:16:27 +0530

    new code

--- src/main/java/leetcode/dp/medium/HouseThief.java
+++ src/main/java/leetcode/dp/medium/HouseThief.java
@@ -9,16 +9,17 @@ public class HouseThief {
   public int rob(int[] nums) {
     int[] dp = new int[nums.length];
     Arrays.fill(dp, -1);
-    return topDown(nums, 0, 0, dp);
+    return topDown(nums, 0, dp);
   }
 
-  private int topDown(int[] nums, int index, int sum, int[] dp) {
+  private int topDown(int[] nums, int index, int[] dp) {
     if (index >= nums.length) {
-      return sum;
+      return 0;
     }
     if (dp[index] == -1) {
-      dp[index] = Math.max(topDown(nums, index + 2, sum + nums[index], dp),
-          topDown(nums, index + 1, sum, dp));
+      int sum1 = nums[index] + topDown(nums, index + 2, dp);
+      int sum2 = topDown(nums, index + 1, dp);
+      dp[index] = Math.max(sum1, sum2);
     }
     return dp[index];
   }
@@ -27,5 +28,6 @@ public class HouseThief {
     HouseThief util = new HouseThief();
     System.out.println(util.rob(new int[] {1, 2, 3, 1}));
     System.out.println(util.rob(new int[] {2, 7, 9, 3, 1}));
+    System.out.println(util.rob(new int[] {1, 2, 1, 1}));
   }
 }
