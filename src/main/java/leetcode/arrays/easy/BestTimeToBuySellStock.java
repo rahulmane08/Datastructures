122ce3e3e2c566b24847157559af4c412a3d7523
rahul.mane08@gmail.com <rahul.mane08@gmail.com>
2024-09-30 23:23:56 +0530

    new code

---
+++ src/main/java/leetcode/arrays/easy/BestTimeToBuySellStock.java
@@ -0,0 +1,30 @@
+package leetcode.arrays.easy;
+
+public class BestTimeToBuySellStock {
+  public int maxProfit(int[] prices) {
+    int maxProfit = 0;
+    int buy = 0;
+    int sell = 1;
+    while (sell < prices.length) {
+      if (prices[buy] >= prices[sell]) {
+        buy = sell;
+      } else {
+        maxProfit = Math.max(maxProfit, prices[sell] - prices[buy]);
+      }
+      sell++;
+    }
+    return maxProfit;
+  }
+
+  public static void main(String[] args) {
+    int[] prices = new int[] {7, 2, 5, 1, 7, 4};
+    BestTimeToBuySellStock util = new BestTimeToBuySellStock();
+    System.out.println(util.maxProfit(prices));
+    prices = new int[] {7, 5, 4, 3, 2};
+    System.out.println(util.maxProfit(prices));
+    prices = new int[] {7, 7, 7, 7, 7};
+    System.out.println(util.maxProfit(prices));
+    prices = new int[] {7, 7, 6, 7, 7};
+    System.out.println(util.maxProfit(prices));
+  }
+}
