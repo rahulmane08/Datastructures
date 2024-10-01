71a4ff80161a7873ec855b8942b255d906efce44
rahul.mane08@gmail.com <rahul.mane08@gmail.com>
2024-10-01 13:08:36 +0530

    new code

--- src/main/java/leetcode/arrays/medium/MaximumGoodSubarraySum.java
+++ src/main/java/leetcode/arrays/medium/MaximumGoodSubarraySum.java
@@ -1,20 +1,63 @@
 package leetcode.arrays.medium;
 
-public class MaximumGoodSubarraySum {
-  public long maximumSubarraySum(int[] nums, int k) {
-    int i = 0;
-    int sum = 0;
-    int start = 0;
-    int end = 0;
-    while (end < nums.length) {
+import java.util.LinkedHashMap;
+import java.util.Map;
 
+public class MaximumGoodSubarraySum {
+  /**
+   * nums = [1,2,3,4,5,6], k = 1
+   * nums = [-1,3,2,4,5], k = 3
+   *
+   * @param nums
+   * @param k
+   * @return
+   */
+  public long maximumSubarraySum(int[] nums, int k) {
+    long maxSubarraySum = getMaxSubarraySum(nums, k);
+    return maxSubarraySum != Long.MIN_VALUE ? maxSubarraySum : 0;
+  }
+
+  private void calculateMaximumSubarraySumTLE(int[] nums, int k, int start, int end, int arraySum, int[] max) {
+    if (end < nums.length && Math.abs(nums[end] - nums[start]) == k) {
+      max[0] = Math.max(arraySum + nums[end], max[0]);
     }
-    return sum;
+    if (end < nums.length) {
+      calculateMaximumSubarraySumTLE(nums, k, start, end + 1, arraySum + nums[end], max);
+    }
+    if (start < end) {
+      calculateMaximumSubarraySumTLE(nums, k, start + 1, end, arraySum - nums[start], max);
+    }
+  }
+
+  private long getMaxSubarraySum(int[] nums, int k) {
+    long maxSubarraySum = Long.MIN_VALUE;
+    Map<Integer, Long> prefixSum = new LinkedHashMap<>();
+    int currentSum = 0;
+    for (int num : nums) {
+      int finalCurrentSum = currentSum;
+      prefixSum.compute(num, (n, sum) -> sum == null ? finalCurrentSum : Math.min(sum, finalCurrentSum));
+      currentSum += num;
+      int lower = num - k;
+      int higher = num + k;
+      if (prefixSum.containsKey(lower)) {
+        maxSubarraySum = Math.max(maxSubarraySum, currentSum - prefixSum.get(lower));
+      }
+      if (prefixSum.containsKey(higher)) {
+        maxSubarraySum = Math.max(maxSubarraySum, currentSum - prefixSum.get(higher));
+      }
+    }
+    return maxSubarraySum;
   }
 
   public static void main(String[] args) {
     MaximumGoodSubarraySum util = new MaximumGoodSubarraySum();
     int[] nums = new int[] {1, 2, 3, 4, 5, 6};
+    System.out.println(util.maximumSubarraySum(nums, 1));
+    nums = new int[] {-1, 3, 2, 4, 5};
+    System.out.println(util.maximumSubarraySum(nums, 3));
+    nums = new int[] {-1, -2, -3, -4};
+    System.out.println(util.maximumSubarraySum(nums, 2));
+    nums = new int[] {1, 5};
     System.out.println(util.maximumSubarraySum(nums, 2));
   }
 }
