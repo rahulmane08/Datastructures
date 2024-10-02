1e9e84446769505efffb95e99426b552aff5e6f9
rahul.mane08@gmail.com <rahul.mane08@gmail.com>
2024-10-02 18:48:31 +0530

    new code

--- src/main/java/leetcode/graph/medium/AllPathsFromSourceLeadToDestination.java
+++ src/main/java/leetcode/graph/medium/AllPathsFromSourceLeadToDestination.java
@@ -2,12 +2,66 @@ package leetcode.graph.medium;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
 public class AllPathsFromSourceLeadToDestination {
+
+  public boolean leadsToDestination(int n, int[][] edges, int source, int destination) {
+    Map<Integer, List<Integer>> graph = new HashMap<>();
+
+    Arrays.stream(edges)
+        .forEach(edge -> {
+          graph.compute(edge[0], (v, list) -> list == null ? new ArrayList<>() : list).add(edge[1]);
+          graph.compute(edge[1], (v, list) -> list == null ? new ArrayList<>() : list);
+        });
+    return leadsToDestination(graph, source, destination, new HashSet<>(), new HashSet<>());
+  }
+
+  /**
+   * 1-----> 2-----> 3*
+   *
+   * @param graph
+   * @param curr
+   * @param destination
+   * @param ongoing
+   * @param visited
+   * @return
+   */
+  boolean leadsToDestination(Map<Integer, List<Integer>> graph,
+                             int curr,
+                             int destination,
+                             HashSet<Integer> ongoing,
+                             HashSet<Integer> visited) {
+    ongoing.add(curr);
+    boolean leadsToDestination = true;
+    if (graph.getOrDefault(curr, Collections.emptyList()).isEmpty()) {
+      leadsToDestination = (curr == destination);
+    } else {
+      for (int neighbor : graph.get(curr)) {
+        if (visited.contains(neighbor)) {
+          continue;
+        }
+
+        if (ongoing.contains(neighbor)) {
+          return false; // cycle present
+        }
+
+        leadsToDestination = leadsToDestination(graph, neighbor, destination, ongoing, visited);
+        if (!leadsToDestination) {
+          return false;
+        }
+      }
+    }
+
+    ongoing.remove(curr);
+    visited.add(curr);
+    return leadsToDestination; // check terminal node is destination
+  }
+
   public static void main(String[] args) {
     int[][] edges = {{0, 1}, {0, 2}, {1, 3}, {2, 3}};
     AllPathsFromSourceLeadToDestination util = new AllPathsFromSourceLeadToDestination();
@@ -18,37 +72,9 @@ public class AllPathsFromSourceLeadToDestination {
     System.out.println(util.leadsToDestination(3, edges, 0, 2));
     edges = new int[][] {{0, 1}, {1, 1}};
     System.out.println(util.leadsToDestination(2, edges, 0, 1));
-  }
-
-  public boolean leadsToDestination(int n, int[][] edges, int source, int destination) {
-    Map<Integer, List<Integer>> graph = new HashMap<>();
-    Map<Integer, Integer> inDegrees = new HashMap<>();
-
-    Arrays.stream(edges)
-        .forEach(edge -> {
-          graph.compute(edge[0], (v, list) -> list == null ? new ArrayList<>() : list).add(edge[1]);
-          inDegrees.compute(edge[1], (v, degree) -> degree == null ? 1 : degree + 1);
-        });
-    return compute(graph, source, destination, new HashSet<>());
-  }
-
-  // wrong
-  boolean compute(Map<Integer, List<Integer>> graph, int curr, int destination, HashSet<Integer> visited) {
-    if (visited.contains(curr)) {
-      return false;
-    }
-
-    visited.add(curr);
-    List<Integer> neighbors = graph.getOrDefault(curr, new ArrayList<>());
-    if (neighbors.isEmpty()) {
-      return curr == destination;
-    }
-
-    for (Integer neighbor : neighbors) {
-      if (!compute(graph, neighbor, destination, visited)) {
-        return false;
-      }
-    }
-    return true;
+    edges = new int[][] {{0, 1}, {0, 2}, {1, 3}, {2, 3}, {1, 4}};
+    System.out.println(util.leadsToDestination(5, edges, 0, 3));
+    edges = new int[][] {{0, 1}, {0, 2}, {1, 3}, {2, 3}, {1, 4}, {4, 3}};
+    System.out.println(util.leadsToDestination(6, edges, 0, 3));
   }
 }
