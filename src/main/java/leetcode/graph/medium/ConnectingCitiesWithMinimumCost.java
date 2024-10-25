e0d87037a12f0a16acbd548040d917d720f5bd89
rahul.mane08@gmail.com <rahul.mane08@gmail.com>
2024-10-25 12:15:58 -0400

    new code

--- src/main/java/leetcode/graph/medium/ConnectingCitiesWithMinimumCost.java
+++ src/main/java/leetcode/graph/medium/ConnectingCitiesWithMinimumCost.java
@@ -1,16 +1,29 @@
 package leetcode.graph.medium;
 
-import java.util.List;
-import leetcode.graph.KruskalMST;
+import datastructures.advanced.DisjointSet;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class ConnectingCitiesWithMinimumCost {
   public int minimumCost(int n, int[][] connections) {
-    int edges = connections.length;
-    if (edges < n - 1) {
+    // sort array by min weights
+    Arrays.sort(connections, Comparator.comparingInt(a -> a[2]));
+    int minCost = 0;
+    DisjointSet<Integer> set = new DisjointSet<>();
+    for (int[] edge : connections) {
+      Integer parent1 = set.findSet(edge[0]);
+      Integer parent2 = set.findSet(edge[1]);
+      if (parent1 == parent2) {
+        continue;
+      }
+      set.union(edge[0], edge[1]);
+      minCost += edge[2];
+    }
+
+    if (set.getCount() != 1 || n > set.getSize()) {
       return -1;
     }
-    KruskalMST mst = new KruskalMST();
-    List<int[]> minPaths = mst.mst(connections);
-    return minPaths.stream().map(a -> a[2]).mapToInt(Integer::intValue).sum();
+
+    return minCost;
   }
 }
