5cd22fdef22a62375f8531eedf506d84d6432ae1
rahul.mane08@gmail.com <rahul.mane08@gmail.com>
2025-01-13 18:49:13 +0530

    new code

--- src/main/java/leetcode/graph/easy/CheckIfGraphIsConnected.java
+++ src/main/java/leetcode/graph/easy/CheckIfGraphIsConnected.java
@@ -3,22 +3,15 @@ package leetcode.graph.easy;
 import datastructures.advanced.DisjointSet;
 
 public class CheckIfGraphIsConnected {
-  public boolean check(int[][] edges) {
-    DisjointSet set = new DisjointSet();
-    for (int[] edge : edges) {
-      set.union(edge[0], edge[1]);
-    }
-    return set.getCount() == 1;
-  }
 
-  public boolean check1(int[][] edges) {
+  public boolean check(int[][] edges) {
     DisjointSet<Integer> set = new DisjointSet<>();
-    for (int [] edge : edges) {
+    for (int[] edge : edges) {
       set.makeSet(edge[0]);
       set.makeSet(edge[1]);
     }
 
-    for (int [] edge : edges) {
+    for (int[] edge : edges) {
       Integer p1 = set.findSet(edge[0]);
       Integer p2 = set.findSet(edge[1]);
       if (p1 == p2) {
--- src/main/java/leetcode/graph/easy/CheckIfGraphIsTree.java
+++ src/main/java/leetcode/graph/easy/CheckIfGraphIsTree.java
@@ -1,16 +1,11 @@
 package leetcode.graph.easy;
 
-import static leetcode.graph.CycleDetectionUtil.detectCycle;
-
 /**
  * 1. Check if no cycles.
  * 2. Start from vertex and check if all vertices are traced.
  */
 public class CheckIfGraphIsTree {
-  public boolean check(int[][] edges, boolean directed) {
-    if (!detectCycle(edges, directed)) {
-      return false;
-    }
-    return new CheckIfGraphIsConnected().check(edges, directed);
+  public boolean check(int[][] edges) {
+    return new CheckIfGraphIsConnected().check(edges);
   }
 }
